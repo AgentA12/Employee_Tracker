3a139020f4c51d6b034d2289b3ee07f2aa038008
Andrew Howran <andrewhowrandev17@gmail.com>
2022-05-09 15:07:28 -0400

    functions to prompt user to add empolyee, role and department

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const inquirer = require("inquirer");
 const ctable = require("console.table");
-// const Connection = require("mysql2/typings/mysql/lib/Connection");
 const mysql = require("mysql2");
-// const db = require("/lib/connection.js");
-// console.table([
-//   {
-//     name: "foo",
-//     age: 10,
-//   },
-//   {
-//     name: "bar",
-//     age: 20,
-//   },
-// ]);
 
 const db = mysql.createConnection(
   {
@@ -23,69 +11,123 @@ const db = mysql.createConnection(
   console.log("jacked in!")
 );
 
-//view all tables-----
-db.query(`SELECT * FROM departments`, (err, results) => {
-  if (err) console.log(err);
-  console.log("Departments:", results);
-});
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "Choice",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all employees",
+          "View all roles",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((answer) => {
+      getAction(answer.Choice);
+    });
+}
 
-db.query(`SELECT * FROM role`, (err, results) => {
-  if (err) console.log(err);
-  console.log("Roles:", results);
-});
+function getAction(action) {
+  if (action.includes("View")) {
+    ViewData(action);
+  } else if (action.includes("Add")) {
+    AddData(action);
+  } else UpdateEmpoyee(action);
+}
 
-db.query(`SELECT * FROM employee`, (err, results) => {
-  if (err) console.log(err);
-  console.log("Employees:", results);
-});
+function ViewData(params) {
+  if (params === "View all departments") {
+    db.query(`SELECT * FROM departments`, (err, results) => {});
+  } else if (params === "View all employees") {
+    db.query(`SELECT * FROM employee`, (err, results) => {});
+  } else db.query(`SELECT * FROM role`, (err, results) => {});
+}
 
-//add role
-db.query(
-  `INSERT INTO role (title, salary, department_id)
-VALUES (?,?,?)`,
-  (err, results) => {
-    if (err) console.log(err);
-    console.log(results);
-  }
-);
+function AddData(params) {
+  if (params === "Add a department") {
+    adddepartment();
+  } else if (params === "Add a role") {
+    addRole();
+  } else addEmployee();
+}
 
-// inquirer
-//   .prompt([
-//     {
-//       type: "list",
-//       name: "Choice",
-//       message: "What would you like to do?",
-//       choices: [
-//         "View all departments",
-//         "View all employees",
-//         "View all roles",
-//         "Add a department",
-//         "Add a role",
-//         "Add an employee",
-//         "Update an employee role",
-//       ],
-//     },
-//   ])
-//   .then((answer) => {
-//     getAction(answer.Choice);
-//   });
+function adddepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "addDepartment",
+        message: "Enter the name of the department:",
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      promptUser();
+    });
+}
 
-// function getAction(action) {
-//   if (action.includes("View")) {
-//     ViewData(action);
-//   } else if (action.includes("Add")) {
-//     AddData(action);
-//   } else UpdateEmpoyee(action);
-// }
+function addRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "Enter the name of the role:",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "Enter the salary of the role:",
+      },
+      {
+        type: "input",
+        name: "roleDepartment",
+        message: "Enter the department of the role:",
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      promptUser();
+    });
+}
 
-// function ViewData(params) {
-//   console.log(params, "view data");
-// }
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "employeeFirstName",
+        message: "Enter the first name of the employee:",
+      },
+      {
+        type: "input",
+        name: "employeeLastName",
+        message: "Enter the last name of the employee:",
+      },
+      {
+        type: "input",
+        name: "employeeRole",
+        message: "Enter the role of the employee:",
+      },
+      {
+        type: "input",
+        name: "employeeManager",
+        message: "Enter the manager of the employee:",
+      },
+    ])
+    .then((res) => {
+      console.log(res);
+      promptUser();
+    });
+}
 
-// function AddData(params) {
-//   console.log(params, "add data");
-// }
+function UpdateEmpoyee() {}
 
-// function UpdateEmpoyee(params) {
-//   console.log(params, " update data");
-// }
+promptUser();
